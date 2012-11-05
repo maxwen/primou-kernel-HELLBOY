cbfb945b6775cbcfc30fc5da512cea50d105a050
maxl <maxl@4b8ad20e-30ca-41b1-81b8-3c8b9e269e15>
2012-11-05 21:15:50 +0000

    changed to new msm kgsl drivers

--- include/linux/msm_kgsl.h
+++ include/linux/msm_kgsl.h
@@ -62,6 +62,7 @@ enum kgsl_user_mem_type {
 	KGSL_USER_MEM_TYPE_ASHMEM	= 0x00000001,
 	KGSL_USER_MEM_TYPE_ADDR		= 0x00000002,
 	KGSL_USER_MEM_TYPE_ION		= 0x00000003,
+	KGSL_USER_MEM_TYPE_MAX		= 0x00000004,
 };
 
 struct kgsl_devinfo {
@@ -150,7 +151,6 @@ struct kgsl_version {
 #define KGSL_2D1_REG_MEMORY	"kgsl_2d1_reg_memory"
 #define KGSL_2D1_IRQ		"kgsl_2d1_irq"
 
-#if defined(CONFIG_MSM_KGSL_ADRENO200) || defined(CONFIG_MSM_KGSL_ADRENO205) || defined(CONFIG_MSM_KGSL_ADRENO220)
 struct kgsl_device_platform_data {
 	struct kgsl_pwrlevel pwrlevel[KGSL_MAX_PWRLEVELS];
 	int init_level;
@@ -164,35 +164,6 @@ struct kgsl_device_platform_data {
 	const char *iommu_user_ctx_name;
 	const char *iommu_priv_ctx_name;
 };
-#else
-struct kgsl_grp_clk_name {
-	const char *clk;
-	const char *pclk;
-};
-
-struct kgsl_device_pwr_data {
-	struct kgsl_pwrlevel pwrlevel[KGSL_MAX_PWRLEVELS];
-	int init_level;
-	int num_levels;
-	int (*set_grp_async)(void);
-	unsigned int idle_timeout;
-	unsigned int nap_allowed;
-};
-
-struct kgsl_clk_data {
-	struct kgsl_grp_clk_name name;
-	struct msm_bus_scale_pdata *bus_scale_table;
-};
-
-struct kgsl_device_platform_data {
-	struct kgsl_device_pwr_data pwr_data;
-	struct kgsl_clk_data clk;
-	/* imem_clk_name is for 3d only, not used in 2d devices */
-	struct kgsl_grp_clk_name imem_clk_name;
-	const char *iommu_user_ctx_name;
-	const char *iommu_priv_ctx_name;
-};
-#endif
 
 #endif
 
